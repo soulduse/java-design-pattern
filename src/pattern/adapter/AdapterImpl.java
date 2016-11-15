dd332d5a494969d4a3b181a9cf890cee50717632
soulduse <soulduse@naver.com>
2016-11-15 22:56:44 +0900

    추가 요구사항에 맞춰 코드 변경 및 결론

--- src/pattern/adapter/AdapterImpl.java
+++ src/pattern/adapter/AdapterImpl.java
@@ -6,10 +6,16 @@ package pattern.adapter;
 public class AdapterImpl implements Adapter{
 
     public Float twiceOf(Float number){
-        return (float)Math.twoTime(number.doubleValue());
+        return Math.doubled(number.doubleValue()).floatValue();
     }
 
+    /* Math 클래스에서 로그를 찍는 기능을 추가 할 수도 있지만,
+       보통의 경우 Math 클래스는 라이브러리 형태로 배포가 되어
+       일반 사용자들이 모두 사용하고 있을테고, 우리만 테스트 해보기 위해선
+       Impl에 하는 것이 일반적이다.
+     */
     public Float halfOf(Float number){
+        System.out.println("절반 구하는 함수");
         return (float)Math.half(number.doubleValue());
     }
 }
